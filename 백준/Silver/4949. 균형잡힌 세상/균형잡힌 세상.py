58ff1e55807cfb5bae9b09a6469831d014c8b8f1
SeongHoon Lee <150895257+seonghoonL@users.noreply.github.com>
2025-04-18 20:56:51 +0900

    [Silver IV] Title: 균형잡힌 세상, Time: 64 ms, Memory: 32412 KB -BaekjoonHub

---
+++ 백준/Silver/4949. 균형잡힌 세상/균형잡힌 세상.py
@@ -0,0 +1,24 @@
+import sys
+while True:
+    line = sys.stdin.readline().rstrip()
+    if line == '.':
+        break
+    stack = []
+    balanced = True
+    for char in line:
+        if char in '([':
+            stack.append(char)
+        elif char == ')':
+            if not stack or stack[-1] != '(':
+                balanced = False
+                break
+            stack.pop()
+        elif char == ']':
+            if not stack or stack[-1] != '[':
+                balanced = False
+                break
+            stack.pop()
+    if balanced and not stack:
+        print("yes")
+    else:
+        print("no")
