d1a81dbb08d7ad7f76fdfef69db4eaf6486ff5b1
SeongHoon Lee <150895257+seonghoonL@users.noreply.github.com>
2025-04-20 20:15:29 +0900

    [Silver II] Title: 알고리즘 수업 - 깊이 우선 탐색 2, Time: 500 ms, Memory: 71096 KB -BaekjoonHub

---
+++ 백준/Silver/24480. 알고리즘 수업 － 깊이 우선 탐색 2/알고리즘 수업 － 깊이 우선 탐색 2.py
@@ -0,0 +1,24 @@
+import sys
+sys.setrecursionlimit(10**6)
+input = sys.stdin.readline
+n, m, r = map(int, input().split())
+graph = [[] for _ in range(n + 1)]
+visited = [False] * (n + 1)
+answer = [0] * (n + 1)
+cnt = 1
+def dfs(v):
+    global cnt
+    visited[v] = True
+    answer[v] = cnt
+    graph[v].sort(reverse=True)
+    for i in graph[v]:
+        if not visited[i]:
+            cnt += 1
+            dfs(i)
+for _ in range(m):
+    x, y = map(int, sys.stdin.readline().split())
+    graph[x].append(y)
+    graph[y].append(x)
+dfs(r)
+for val in answer[1:]:
+    print(val)
