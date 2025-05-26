b4d06802f37b0c2dcbfefe112874ffc2bb886d55
SeongHoon Lee <150895257+seonghoonL@users.noreply.github.com>
2025-05-26 20:52:34 +0900

    [Gold V] Title: ABCDE, Time: 1216 ms, Memory: 33432 KB -BaekjoonHub

---
+++ 백준/Gold/13023. ABCDE/ABCDE.py
@@ -0,0 +1,27 @@
+import sys
+sys.setrecursionlimit(10**6)
+n,m = map(int,input().split())
+graph = [ [] for _ in range(n)]
+for i in range(m):
+    A,B = map(int,input().split())
+    graph[A].append(B)
+    graph[B].append(A)
+def dfs(x,visited,cnt):
+    global answer
+    if cnt == 4:
+        answer = 1
+        return
+    visited[x] = True
+    for i in graph[x]:
+        if not visited[i]:
+            dfs(i,visited,cnt+1)
+    visited[x] = False
+for i in range(n):
+    answer = 0
+    visited = [False for _ in range(n)]
+    dfs(i,visited,0)
+    if answer == 1:
+        print(answer)
+        break
+else:
+    print(0)
